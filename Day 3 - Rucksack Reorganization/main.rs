a967a4ccb58e50a71f6c72c7863a3dca546a9ac5
1tpp <contact@1tpp.com>
2022-12-04 01:03:25 +0700

    Rucksack Reorganization

---
+++ Day 3 - Rucksack Reorganization/main.rs
@@ -0,0 +1,37 @@
+fn main() {
+    let input_data = include_bytes!("./input.txt");
+
+    // Part 2
+    println!(
+        "{:?}",
+        input_data
+            .split(|byte| *byte == b'\n')
+            .map(|line| line.split_at(line.len() / 2))
+            .map(|(a, b)| 
+                b.iter().filter(|byte| a.contains(byte))
+                .map(|byte| if *byte >= b'a' {
+                    (byte - b'a') as i16 + 1
+                } else {
+                    (byte - b'A') as i16 + 27
+                })
+                .next()
+                .unwrap()
+            ).sum::<i16>(),
+    );
+
+    // Part 2
+    println!(
+        "{:?}",
+        input_data
+            .split(|byte| *byte == b'\n')
+            .collect::<Vec<_>>()
+            .chunks(3)
+            .map(|line| line[0].iter().filter(|byte| line[1].contains(byte) && line[2].contains(byte)).next().unwrap())
+            .map(|byte| if *byte >= b'a' {
+                (byte - b'a') as i16 + 1
+            } else {
+                (byte - b'A') as i16 + 27
+            })
+            .sum::<i16>(),
+    );
+}
