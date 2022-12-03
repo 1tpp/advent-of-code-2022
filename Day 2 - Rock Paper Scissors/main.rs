419e58d9c8bb14eb6c0f0010baf986f27a9edcf5
1tpp <contact@1tpp.com>
2022-12-04 01:06:24 +0700

    Rock Paper Scissors

--- Day 2 - Rock Paper Scissors/main.rs
+++ Day 2 - Rock Paper Scissors/main.rs
@@ -4,8 +4,8 @@ fn main() {
     println!(
         "{}",
             input_data
-            .split(|b| *b == b'\n')
-            .map(|l| ((l[0] - b'A') as i16, (l[2] - b'X') as i16,))
+            .split(|byte| *byte == b'\n')
+            .map(|line| ((line[0] - b'A') as i16, (line[2] - b'X') as i16,))
             .map(|(a, b)| 1 + b + 3 * ((1 + b - a).rem_euclid(3)))
             .sum::<i16>(),
     );
@@ -14,8 +14,8 @@ fn main() {
     println!(
         "{}",
             input_data
-            .split(|b| *b == b'\n')
-            .map(|l| ((l[0] - b'A') as i16, (l[2] - b'X') as i16,))
+            .split(|byte| *byte == b'\n')
+            .map(|line| ((line[0] - b'A') as i16, (line[2] - b'X') as i16,))
             .map(|(a, b)| 1 + b * 3 + (2 + a + b) % 3)
             .sum::<i16>(),
     );
