d0376c107076877b19020f32d08737f8128f3d4c
1tpp <contact@1tpp.com>
2022-12-03 23:56:40 +0700

    chore: change name variable

--- Day 1 - Calorie Counting/main.rs
+++ Day 1 - Calorie Counting/main.rs
@@ -1,11 +1,11 @@
 fn main() {
-  let input_text = include_str!("./input.txt");
+  let input_data = include_str!("./input.txt");
 
   // part 1
-  println!("{}",input_text.split("\n\n").map(|e| e.lines().map(|c| c.parse::<u32>().unwrap()).sum::<u32>()).max().unwrap(),);
+  println!("{}",input_data.split("\n\n").map(|e| e.lines().map(|c| c.parse::<u32>().unwrap()).sum::<u32>()).max().unwrap(),);
   
   // part 2
-  let mut calories = input_text.split("\n\n").map(|e| e.lines().map(|c| c.parse::<u32>().unwrap()).sum()).collect::<Vec<u32>>();
+  let mut calories = input_data.split("\n\n").map(|e| e.lines().map(|c| c.parse::<u32>().unwrap()).sum()).collect::<Vec<u32>>();
   calories.sort_unstable();
   println!("{}", calories.into_iter().rev().take(3).sum::<u32>());
 }
